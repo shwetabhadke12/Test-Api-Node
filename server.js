8cbb0585d04d6465542e2ca39a895f3b6452a9c8
shwetabhadke12 <shweta.bhadke@accelgrowthtech.com>
2024-02-26 23:31:09 +0530

    added atachments

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
